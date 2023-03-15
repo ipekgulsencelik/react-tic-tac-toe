3d3432c84387539c98276d6357c2a67adab8fdd1
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-15 20:23:55 +0300

    adding gameboard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import Cell from "./components/cell/Cell";
 const App = () => {
   return (
     <div className="App">
-      <Cell />
+      <div className="gameboard">
+        <Cell />
+      </div>
+      <p></p>
     </div>
   );
 };
