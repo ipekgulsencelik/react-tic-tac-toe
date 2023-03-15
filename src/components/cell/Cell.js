a42dd3325651e3a91840818ea966a54cb8b36236
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-15 21:51:19 +0300

    adding cell's taken control

--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -1,7 +1,27 @@
-import style from "./style.module.scss";
+import "./style.scss";
 
-const Cell = ({ id, Cell }) => {
-  return <div className={`${style["square"]}`} id={id}></div>
+const Cell = ({ id, Cell, setCells, go }) => {
+  const handleClick = (e) => {
+    console.log(e.target);
+    const taken =
+      e.target.firstChild.classList.contains("circle") ||
+      e.target.firstChild.classList.contains("cross");
+
+    if (!taken) {
+      if (go === "circle") {
+        e.target.firstChild.classList.add("circle");
+      }
+      if (go === "cross") {
+        e.target.firstChild.classList.add("cross");
+      }
+    }
+  };
+
+  return (
+    <div className="square" id={id} onClick={handleClick}>
+      <div className="cell"></div>
+    </div>
+  );
 };
 
 export default Cell;
