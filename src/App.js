e0300db3f75b36287c3c157c42b70719992f064f
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-15 20:47:13 +0300

    adding cells

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 import Cell from "./components/cell/Cell";
 
 const App = () => {
+  const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  console.log(cells);
   return (
     <div className="App">
       <div className="gameboard">
-        <Cell />
+        {cells.map((cell, index) => (
+          <Cell key={index} id={index} cell={cell} />
+        ))}
       </div>
       <p></p>
     </div>
--- src/components/cell/Cell.js
+++ src/components/cell/Cell.js
@@ -1,7 +1,7 @@
 import style from "./style.module.scss";
 
-const Cell = () => {
-  return <div className={`${style["square"]}`}></div>;
+const Cell = ({ id, Cell }) => {
+  return <div className={`${style["square"]}`} id={id}></div>
 };
 
 export default Cell;
